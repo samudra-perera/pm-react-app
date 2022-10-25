97173b5347f3becd79d73d6577dc4d70b4497662
Samudra Perera <samudrapup@gmail.com>
2022-10-25 19:39:18 -0400

    Added GraphQL Schema for local Querying

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-require('dotenv').config()
-const { graphqlHTTP } = require('express-graphql')
-const port = process.env.PORT || 5000
-const schema = require('../server/schema/schema')
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const port = process.env.PORT || 5000;
+const schema = require("../server/schema/schema");
+const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
-app.listen(port, console.log(`Server is running on port ${port}`))
+app.listen(port, console.log(`Server is running on port ${port}`));
