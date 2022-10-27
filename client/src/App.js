7e3b582c05cbc89c04bca4421e1fa1c099550e50
Samudra Perera <samudrapup@gmail.com>
2022-10-27 15:20:02 -0400

    Created Client List and Project Components + GraphQL Query Requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
 import Header from "./components/Header";
+import Clients from "./components/Clients";
+import Projects from "./components/Projects";
+import AddClientModal from "./components/AddClientModal";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+      }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  uri: "http://localhost:2121/graphql",
+  cache,
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <AddClientModal/>
+          <Clients/>
+          <Projects/>
+        </div>
+      </ApolloProvider>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require('colors')
+const cors = require('cors')
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const app = express();
 
 //Connect to DB
 connectDB()
+
+app.use(cors())
 
 app.use(
   "/graphql",
